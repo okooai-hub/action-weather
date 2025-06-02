f50a6231f83d272185d979841fd04ed5fdb3842e
boilpy <achillesrasquinha@gmail.com>
2025-06-02 09:05:47 -0500

    feat: add action

---
+++ action.py
@@ -0,0 +1,9 @@
+import agentflow as af
+
+async def handle(**kwargs):
+    longitude = af.param('longitude')
+    latitude  = af.param('latitude')
+
+    return await af.request('https://api.open-meteo.com/v1/forecast',
+        params = { 'longitude': longitude, 'latitude': latitude,
+            'current_weather': True })
