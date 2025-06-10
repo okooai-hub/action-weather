19dc4e2746d4aa19a31dfdf6b2bc9f99a731bcd1
Achilles Rasquinha <achillesrasquinha@gmail.com>
2025-06-10 09:58:37 -0500

    Update action.py

--- action.py
+++ action.py
@@ -4,6 +4,6 @@ async def handle():
     longitude = af.param("longitude")
     latitude  = af.param("latitude")
 
-    return await af.request('https://api.open-meteo.com/v1/forecast',
+    return await af.arequest('https://api.open-meteo.com/v1/forecast',
         params = { 'longitude': longitude, 'latitude': latitude,
             'current_weather': True })
